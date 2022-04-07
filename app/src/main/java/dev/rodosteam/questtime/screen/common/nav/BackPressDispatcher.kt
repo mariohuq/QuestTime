947204392fb3a7a915496663965c1d82a03c8da5
done_bd <dinil.bd@mail.ru>
2022-04-08 00:05:35 +0300

    delete backstack dispatcher

--- app/src/main/java/dev/rodosteam/questtime/screen/common/nav/BackPressDispatcher.kt
+++
@@ -1,8 +0,0 @@
-package dev.rodosteam.questtime.screen.common.nav
-
-interface BackPressDispatcher {
-
-    fun registerListener(listener: BackPressedListener)
-    fun unregisterListener(listener: BackPressedListener)
-
-}
