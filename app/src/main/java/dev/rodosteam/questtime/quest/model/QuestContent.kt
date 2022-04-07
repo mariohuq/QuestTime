7e0c784c65cadf1540fa3002c8868b9ef0851cf3
mariohuq <mariohuq@github.com>
2022-04-07 14:04:03 +0300

    Add QuestContent, Walkthrough models

--- app/src/main/java/dev/rodosteam/questtime/quest/model/QuestContent.kt
+++ app/src/main/java/dev/rodosteam/questtime/quest/model/QuestContent.kt
@@ -1,6 +1,22 @@
 package dev.rodosteam.questtime.quest.model
 
-//TODO
-class QuestContent(val someTreeMap: Collection<Int>) {
+class QuestContent private constructor(val pages: Map<Page.Id, Page>, val startingId: Page.Id) {
+    constructor(pages: Iterable<Page>, startingId: Page.Id) : this(
+        pages.associateBy { it.id },
+        startingId
+    )
 
+    data class Page(
+        val id: Id,
+        val displayText: String,
+        val choices: List<Choice>
+    ) {
+        @JvmInline
+        value class Id(private val id: Long)
+
+        data class Choice(
+            val targetId: Id,
+            val displayText: String
+        )
+    }
 }
