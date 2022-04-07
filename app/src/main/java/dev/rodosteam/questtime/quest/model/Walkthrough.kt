6b193b54814fa2bc106338e2950a8d56966003ea
mariohuq <mariohuq@github.com>
2022-04-07 16:26:14 +0300

    Add doc comments to Walkthrough and QuestContent

--- app/src/main/java/dev/rodosteam/questtime/quest/model/Walkthrough.kt
+++ app/src/main/java/dev/rodosteam/questtime/quest/model/Walkthrough.kt
@@ -1,15 +1,37 @@
 package dev.rodosteam.questtime.quest.model
 
+/**
+ * Состояние прохождения квеста.
+ */
 class Walkthrough private constructor(
+    // квест
     private val content: QuestContent,
+    // история прохождения, чтобы возвращаться назад
     private val history: List<QuestContent.Page.Id>
 ) {
+    /**
+     * Начинает прохождение данного квеста.
+     */
     constructor(content: QuestContent) : this(content, listOf(content.startingId))
 
+    /**
+     * Current page
+     */
     val page get() = content.pages.getValue(history.last())
 
+    /**
+     * Шаг назад.
+     *
+     * @return возвращает предыдущее состояние
+     */
     fun goBack() = Walkthrough(content, history.dropLast(1))
 
+    /**
+     * Шаг вперед.
+     *
+     * @param index Индекс выбранного варианта на странице
+     * @return возвращает состояние после сделанного выбора
+     */
     fun choose(index: Int) = Walkthrough(
         content,
         history + page.choices[index].targetId
