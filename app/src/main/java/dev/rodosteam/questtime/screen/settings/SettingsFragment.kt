63dffb69af65602ff96b3de019a1b457bbf2c0e6
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2022-05-03 17:51:33 +0300

    Languages added: English, Russian and German

--- app/src/main/java/dev/rodosteam/questtime/screen/settings/SettingsFragment.kt
+++ app/src/main/java/dev/rodosteam/questtime/screen/settings/SettingsFragment.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.lifecycle.Observer
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.core.app.ActivityCompat.recreate
 import androidx.lifecycle.ViewModelProvider
+import dev.rodosteam.questtime.R
 import dev.rodosteam.questtime.databinding.FragmentSettingsBinding
 import dev.rodosteam.questtime.screen.common.base.BaseFragment
+import dev.rodosteam.questtime.utils.LocaleManager.getLanguageCode
+import dev.rodosteam.questtime.utils.LocaleManager.getLanguagePosition
+import dev.rodosteam.questtime.utils.LocaleManager.setLanguageCode
+import dev.rodosteam.questtime.utils.LocaleManager.setLanguagePosition
+
 
 class SettingsFragment : BaseFragment() {
 
@@ -25,20 +33,50 @@ class SettingsFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
+            ViewModelProvider(this)[SettingsViewModel::class.java]
 
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSettings
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        val languagesSpinner : Spinner = binding.languages
+        ArrayAdapter.createFromResource(requireContext(), R.array.languages, android.R.layout.simple_spinner_item).also {
+            adapter -> adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            languagesSpinner.adapter = adapter
+        }
+        languagesSpinner.setSelection(getLanguagePosition(requireContext()))
+        languagesSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when (position) {
+                    0 -> changeLanguageCode("en", 0)
+                    1 -> changeLanguageCode("ru", 1)
+                    2 -> changeLanguageCode("de", 2)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                changeLanguageCode("en", 0)
+            }
+
+        }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun changeLanguageCode(languageCode: String, position: Int) {
+        if (getLanguageCode(requireContext()) != languageCode) {
+            setLanguageCode(requireContext(), languageCode)
+            setLanguagePosition(requireContext(), position)
+            recreate(mainActivity)
+        }
     }
 }
---
+++ app/src/main/java/dev/rodosteam/questtime/utils/LocaleManager.kt
@@ -0,0 +1,47 @@
+package dev.rodosteam.questtime.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import java.util.Locale
+
+
+object LocaleManager {
+
+    fun setLocale(context : Context): Context {
+        return updateResources(context, getLanguageCode(context))
+    }
+
+    fun setLanguageCode(context: Context, code: String) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("languageCodePrefs", Context.MODE_PRIVATE)
+        val sharedPreferencesEditor: SharedPreferences.Editor = sharedPreferences.edit()
+        sharedPreferencesEditor.putString("languageCodeValue", code)
+        sharedPreferencesEditor.apply()
+    }
+
+    fun getLanguageCode(context: Context): String {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("languageCodePrefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("languageCodeValue", "en")!!
+    }
+
+    fun setLanguagePosition(context: Context, position: Int) {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("languageCodePrefs", Context.MODE_PRIVATE)
+        val sharedPreferencesEditor: SharedPreferences.Editor = sharedPreferences.edit()
+        sharedPreferencesEditor.putInt("languagePositionValue", position)
+        sharedPreferencesEditor.apply()
+    }
+
+    fun getLanguagePosition(context: Context): Int {
+        val sharedPreferences: SharedPreferences = context.getSharedPreferences("languageCodePrefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getInt("languagePositionValue", 0)
+    }
+
+    private fun updateResources(context: Context, language: String): Context {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val configuration: Configuration = context.resources.configuration
+        configuration.setLocale(locale)
+        configuration.setLayoutDirection(locale)
+        return context.createConfigurationContext(configuration)
+    }
+}
